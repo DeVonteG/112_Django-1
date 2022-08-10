a8fe7ee09bddad05d241dd216c65970e4983e7c7
DeVonte G <dagray92@hotmail.com>
2022-08-09 22:29:07 -0400

    class commit

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a53vd=j!f*d@1+(-m89ptoen6t8y!c@916*d*!dzdq5=(6z+)4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
